46592a599b6cdf4ef403345257e2237fb69aaa4c
Your Name <you@example.com>
2023-10-01 09:49:49 +0300

    fix code

--- src/CashalotApiServiceProvider.php
+++ src/CashalotApiServiceProvider.php
@@ -13,12 +13,6 @@ class CashalotApiServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'gavan4eg');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'gavan4eg');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
-        // Publishing is only necessary when using the CLI.
         if ($this->app->runningInConsole()) {
             $this->bootForConsole();
         }
@@ -32,11 +26,6 @@ class CashalotApiServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->mergeConfigFrom(__DIR__.'/../config/cashalot.php', 'cashalot');
-
-        // Register the service the package provides.
-        $this->app->singleton('cashalotapi', function ($app) {
-            return new CashalotApi;
-        });
     }
 
     /**
@@ -56,27 +45,8 @@ class CashalotApiServiceProvider extends ServiceProvider
      */
     protected function bootForConsole(): void
     {
-        // Publishing the configuration file.
         $this->publishes([
             __DIR__.'/../config/cashalot.php' => config_path('cashalot.php'),
         ], 'cashalot.config');
-
-        // Publishing the views.
-        /*$this->publishes([
-            __DIR__.'/../resources/views' => base_path('resources/views/vendor/gavan4eg'),
-        ], 'cashalotapi.views');*/
-
-        // Publishing assets.
-        /*$this->publishes([
-            __DIR__.'/../resources/assets' => public_path('vendor/gavan4eg'),
-        ], 'cashalotapi.assets');*/
-
-        // Publishing the translation files.
-        /*$this->publishes([
-            __DIR__.'/../resources/lang' => resource_path('lang/vendor/gavan4eg'),
-        ], 'cashalotapi.lang');*/
-
-        // Registering package commands.
-        // $this->commands([]);
     }
 }
